2e48d4b67c87d7f14e9dce5f86e9add4a0363490
atharvadubeyofficial <24f3003562@ds.study.iitm.ac.in>
2025-12-30 23:21:01 +0530

    Update audio_utils.py

--- utils/audio_utils.py
+++ utils/audio_utils.py
@@ -1,6 +1,22 @@
+import librosa
+import soundfile as sf
+import numpy as np
 import tempfile
 
-def save_audio_temp(uploaded_file):
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp:
-        temp.write(uploaded_file.read())
-        return temp.name
+def extract_suspicious_audio(audio_path, scores, threshold=0.6):
+    if not scores:
+        return None
+
+    idx = np.argmax(scores)
+    if scores[idx] < threshold:
+        return None
+
+    y, sr = librosa.load(audio_path, sr=16000)
+    start = idx * 512
+    end = start + 16000
+
+    suspicious = y[start:end]
+
+    temp = tempfile.NamedTemporaryFile(delete=False, suffix=".wav")
+    sf.write(temp.name, suspicious, sr)
+    return temp.name
